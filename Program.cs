4bfba34d80f88a4450c83421a06a136917dcdfe3
PedroRibeiro91 <55151565+PedroRibeiro91@users.noreply.github.com>
2020-08-25 17:09:19 +0100

    Simple Calculator
    This is a simple calculator. 
    When run the user is prompted to enter two number and an operation sign. The program will return the result.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ENTER A NUMBER: ");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("ENTER THE OPERATION: ");
+            string operation = Console.ReadLine();
+
+            Console.Write("ENTER A NUMBER: ");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            if (operation == "+")
+            {
+                Console.WriteLine(n1 + n2);
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine(n1 - n2);
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine(n1 * n2);
+            }
+            else if (operation == "/")
+            {
+                Console.WriteLine(n1 / n2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operation");
+            }
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
