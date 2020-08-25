3cfc8c6fb562b574816a6aa367b93b90b866dbf8
PedroRibeiro91 <55151565+PedroRibeiro91@users.noreply.github.com>
2020-08-25 14:28:59 +0100

    MadLIb
    This is a simple C# program when run the user is prompted to enter a color, a plural noun and a celebrity name and then gets a 3 sentence story incorportating their inputs.

---
+++ MadLib.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color, pluralNoun, celebrity;
+            Console.Write("Enter a color: ");
+            color = Console.ReadLine();
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+            Console.Write("Enter a celebrity name: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("The sky is " + color);
+            Console.WriteLine(pluralNoun + " are also " + color);
+            Console.WriteLine("You wish " + celebrity + " had " + color + " skin tone");
+            Console.ReadLine();
+
+
+
+
+
+         
+
+
+        }
+
+    }
+}
